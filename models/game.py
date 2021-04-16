580aea4071b8a0d0c4595d480e31902f4082c6d3
Daniel Callus <danielcallus@gmail.com>
2021-04-16 15:52:03 +0100

    added game rps (from previous lab, but refatored to take player as argument and extract move)

---
+++ models/game.py
@@ -0,0 +1,38 @@
+class Game:
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
+        
+
+        self.win_game = [["Rock","Scissors"], ["Paper","Rock"], ["Scissors", "Paper"]]
+
+        """ big bang mode:
+        self.win_game = [["Rock","Scissors"], ["Paper","Rock"], ["Scissors", "Paper"], 
+        ["Rock", "Lizard"], ["Lizard", "Spock"], ["Spock", "Scissors"], ["Scissors", "Lizard"], 
+        ["Lizard", "Paper"], ["Paper", "Spock"], ["Spock", "Rock"]]
+        """
+    
+    def set_the_winner(self, player):
+        self.winner = player
+
+    def return_winner(self):
+        return f"{self.winner.name} wins"
+
+    def test_result(self, player_1, player_2):
+
+        if player_1.gesture == player_2.gesture:
+            return "tie"
+
+        # first checks if player_1 = game[0], then checks if player_2 = game[0]
+        for game in self.win_game:   # example. win_game[0] = ["Rock","Scissors"]
+            if [player_1.gesture, player_2.gesture] == game:  # list = ["Rock","Scissors"]
+                self.set_the_winner(self.player_1)
+                return self.return_winner()
+                
+            elif [player_2.gesture, player_1.gesture] == game: # list = ["Rock","Scissors"]
+                self.set_the_winner(self.player_2)
+                return self.return_winner()
+        
+        return "Please enter valid gesture"
+
+    
---
+++ models/player.py
@@ -0,0 +1,6 @@
+"""Player class, where the value rock, paper or scissors is stored."""
+class Player:
+    def __init__(self, name, gesture):
+        self.name = name
+        self.move = gesture
+
