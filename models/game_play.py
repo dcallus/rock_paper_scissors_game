28f40c2df52ca0ac4c0e0904184580df3984ac22
Daniel Callus <danielcallus@gmail.com>
2021-04-17 09:32:07 +0100

    removed redundant comments

--- models/game_play.py
+++ models/game_play.py
@@ -7,6 +7,5 @@ player_2 = Player('Player 2')
 game_play = Game(player_1, player_2)
 
 def get_results(player_1, player_2):
-
-    result = game_play.test_result(player_1, player_2)  # test winner + return result
+    result = game_play.test_result(player_1, player_2)
     return result
