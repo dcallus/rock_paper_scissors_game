3ac96c15f4b25a0ae603c502ec2db35e359322cf
Daniel Callus <danielcallus@gmail.com>
2021-04-17 11:03:50 +0100

    refactored to avoid repeating code

--- controllers/controller.py
+++ controllers/controller.py
@@ -22,12 +22,15 @@ def show(player_1_gesture, player_2_gesture):
 def play_computer_home():
     return render_template('play_computer.html', title='Rock/Paper/Scissors')
 
-@app.route('/play', methods=['POST'])
-def play_computer_form():
+def process_form():
     user_name = request.form['player-name']
-    user_choice = request.form['rps-menu']
 
-    player_1 = Player(user_name)
+    if user_name:
+        player_1 = Player(user_name)
+    else:
+        player_1 = Player("Player")
+
+    user_choice = request.form['rps-menu']
     player_1.set_gesture(user_choice)
 
     vs_computer_game = Game(player_1)
@@ -36,4 +39,12 @@ def play_computer_form():
 
     winner = get_results(player_1, player_2)
 
+    return (winner, player_1, player_2)
+
+@app.route('/play', methods=['POST'])
+def play_computer():
+    game_data = process_form()
+    winner = (game_data[0])
+    player_1 = (game_data[1])
+    player_2 = (game_data[2])
     return render_template('result.html', title='Rock/Paper/Scissors', winner=winner, player_1=player_1, player_2=player_2)
